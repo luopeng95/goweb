cf971c2625f8973fc529ebf26a1d76bcb28c2ec1
luopeng95 <1913434404@qq.com>
2020-04-12 15:59:04 +0800

    master

--- 小东西/拼图游戏/js/index.js
+++ 小东西/拼图游戏/js/index.js
@@ -3,7 +3,7 @@
             this.leave = +leave;         //定义难度属性
             this.step = 0;              //初始步数
             this.userTime = 0;          //初始时间
-            this.timer;                 //计时的定时器
+            this.timer1;                 //计时的定时器
             this.blank = +leave; //定义初始的空白位置（4*4  5*5）
             this.imgUrl = imgUrl;
             this.parent = parent;
@@ -15,15 +15,17 @@
 
         // 初始化函数
         init(){
+            $("#step").html("0")
             this.timer();
             this.createGrid();
             this.differentOrder();
+            this.clickLi();
         }
 
         // 计时函数
         timer(){
-            setInterval(() => {
-                $("#fun>span").eq(0).html(`${parseInt((this.userTime / 60) % 60)}:${parseInt((this.userTime / 10) % 6)}${parseInt((this.userTime / 1) % 10)}`);
+            this.timer1 = setInterval(() => {
+                $("#timer").eq(0).html(`${parseInt((this.userTime / 60) % 60)}:${parseInt((this.userTime / 10) % 6)}${parseInt((this.userTime / 1) % 10)}`);
                 this.userTime += 1;
             }, 1000);
         }
@@ -43,15 +45,15 @@
             // 拿到空白周围的一个li,将空白格与选中的li进行交换
             // 要准确的交换必须定义一个属性来记录li当前的位置--data-class
             // 打乱50次
-            for(let i = 0; i < 50; ++i){
-                console.log(`第${i}次`);
+            for(let i = 0; i < this.leave * 3; ++i){
+                // console.log(`第${i}次`);
                 let num = this.puzzNum();
                 let oLi = this.returnLi(num);
                 // console.log(oLi);
                 oLi.attr("data-class", this.blank);
-                oLi.css({"top":`${parseInt(this.blank/this.sqret)*500/this.sqret}px`, "left":`${parseInt(this.blank%this.sqret)*500/this.sqret}px`});
+                oLi.css({"top":`${parseInt(this.blank/this.sqret)*500/this.sqret}px`, "left":`${this.blank%this.sqret*500/this.sqret}px`});
                 this.blank = num;
-                console.log(this.blank);
+                // console.log(this.blank);
             }
         }
 
@@ -89,9 +91,62 @@
             }
         }
 
+        // li的点击事件
+        clickLi(){
+            this.parent.on("click", "li", (e) => {
+                console.log(this.blank);
+                let oLi = $(`[data-class=${e.target.getAttribute("data-class")}]`).eq(0);
+                let abs = Math.abs(+oLi.attr("data-class") - this.blank);
+                let max = +oLi.attr("data-class") > this.blank ? +oLi.attr("data-class") : this.blank;
+                if(abs === this.sqret){
+                    oLi.css("top", `${parseInt(this.blank/this.sqret) * 500/this.sqret}px`);
+                    
+                }else if(abs === 1 && max % this.sqret != 0){
+                    oLi.css("left", `${(this.blank%this.sqret)*500/this.sqret}px`)
+                    
+                }else{
+                    return null;
+                }
+                // console.log(new Rule().check());
+                let index = +oLi.attr("data-class");
+                oLi.attr("data-class", this.blank);
+                this.blank = index;
+                ++this.step;
+                if(new Rule().check()){
+                    let ans = confirm("恭喜已经完成拼图, 是否再来一次");
+                    $("#main>li").eq(this.blank).css("display", "block");
+                    if(ans){
+                        location.reload();
+                    }
+                }
+                $("#step").html(`${this.step}`)
+            })
+        }
+
+        // 清除定时器，清空步数，去除点击事件
+        clearTimer(){
+            clearInterval(this.timer1);
+            $("#step").html("0");
+            $("#main").off("click", "li");
+        }
+
+    }
 
 
+    class Rule{
+        constructor(){
+            
+        }
 
-
-
+        // 判断此时是否已经完成拼图
+        check(){
+            let oLis = $("#main>li")
+            for(let i = 0; i < oLis.length; ++i){
+                // console.log(oLis.eq(i).attr("data-id"), oLis.eq(i).attr("data-class"))
+                if(oLis.eq(i).attr("data-id") != oLis.eq(i).attr("data-class")){
+                    return false;
+                }
+            }
+            return true
+        }
     }
