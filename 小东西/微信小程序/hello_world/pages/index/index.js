427223c13f9d62dbf6fc76c906e742f36c9ecea3
luopeng95 <1913434404@qq.com>
2020-05-18 20:51:23 +0800

    2020.5.18小程序第一天

---
+++ 小东西/微信小程序/hello_world/pages/index/index.js
@@ -0,0 +1,17 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    name:"刘德华",
+    englishName:"Andy Liu",
+    nickName:"Andy Liu",
+    nationNality:"中国",
+    nation:"汉族",
+    constellation:"不知道",
+    height:"不知道",
+    obj1:[{id:0,text:"拉取要求"},{id:1,text:"问题"},{id:2,text:"市场"},{id:3,text:"探索"},],
+    arr1:[1,2,3,4,5,6,7,8,9],
+  },
+})
