a567f2ae3dbb4cc57ae6eaa6f3d37f7c72ef470b
luopeng95 <1913434404@qq.com>
2020-06-09 20:58:11 +0800

    toutiao

---
+++ 小东西/toutiao/src/App.vue
@@ -0,0 +1,101 @@
+<template>
+  <div id="app">
+    <div class="lp-navTop">
+      <div class="lp-navTop-left">
+        <div class="lp-navTop-left-download">下载App</div>
+        <div class="lp-navTop-left-reg">注册头条号</div>
+      </div>
+      <div>侵权投诉</div>
+    </div>
+    <!-- 内容区开始 -->
+    <div id="lp-content">
+      <!-- 左边导航开始 -->
+      <navs></navs>
+      <!-- 左边导航结束 -->
+
+      <!-- 中间内容开始 -->
+      <div class="lp-content-mid">2</div>
+      <!-- 中间内容结束 -->
+
+      <!-- 右边内容开始 -->
+      <div class="lp-content-right">
+        <div class="lp-content-right-search">
+          <input type="text">
+          <div>搜索</div>
+        </div>
+      </div>
+      <!-- 右边内容结束 -->
+    </div>
+    <!-- 内容区结束 -->
+  </div>
+</template>
+
+<script>
+import navs from "./components/navs/nav";
+
+export default {
+  name: 'App',
+  components: {
+    navs
+  }
+}
+</script>
+
+<style scoped>
+#app {
+  min-width: 960px;
+  width: 100%;
+}
+.lp-navTop{
+  background-color: black;
+  width: 100%;
+  height: 40px;
+  color: white;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  font-size: 16px;
+  /* padding: 5px 10px; */
+}
+.lp-navTop-left{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.lp-navTop-left-download{
+  margin: 0 15px;
+}
+.lp-navTop-left-reg{
+  margin: 0 15px;
+}
+
+/* 内容区开始 */
+  #lp-content{
+    background-color: darkseagreen;
+    width: 100%;
+    margin: 0 auto;
+    display: flex;
+  }
+
+  /* 中间区域开始 */
+  .lp-content-mid{
+    background-color: darkturquoise;
+    flex: 1 1 0;
+  }
+  /* 中间区域结束 */
+
+  /* 右边开始 */
+  .lp-content-right{
+    background-color: hotpink;
+    flex: 0 0 300px;
+  }
+
+  .lp-content-right-search{
+    display: flex;
+    justify-content: center;
+    margin-top: 20px;
+    align-items: center;
+  }
+  /* 右边结束 */
+/* 内容区结束 */
+</style>
---
+++ 小东西/toutiao/src/components/navs/nav.vue
@@ -0,0 +1,80 @@
+<!--  -->
+<template>
+  <div class='lp-toutiao-nav'>
+      <h4>泽林头条</h4>
+      <li v-for="v,i in lists" :class="{active:i === active}" @click="cliNav($event,i)">
+          {{v}}
+      </li>
+  </div>
+</template>
+
+<script>
+//这里可以导入其他文件（比如：组件，工具js，第三方插件js，json文件，图片文件等等）
+//例如：import 《组件名称》 from '《组件路径》';
+
+export default {
+  //import引入的组件需要注入到对象中才能使用
+  components: {},
+  data() {
+    //这里存放数据
+    return {
+        lists:["推荐","西瓜视频","财经","热点","直播","图片","科技","娱乐","游戏","体育","懂车帝","热点"],
+        active:0
+    };
+  },
+  //监听属性 类似于data概念
+  computed: {},
+  //监控data中的数据变化
+  watch: {},
+  //方法集合
+  methods: {
+      cliNav(e,i){
+          this.active = i;
+      }
+  },
+  //生命周期 - 创建完成（可以访问当前this实例）
+  created() {},
+  //生命周期 - 挂载完成（可以访问DOM元素）
+  mounted() {},
+  beforeCreate() {}, //生命周期 - 创建之前
+  beforeMount() {}, //生命周期 - 挂载之前
+  beforeUpdate() {}, //生命周期 - 更新之前
+  updated() {}, //生命周期 - 更新之后
+  beforeDestroy() {}, //生命周期 - 销毁之前
+  destroyed() {}, //生命周期 - 销毁完成
+  activated() {} //如果页面有keep-alive缓存功能，这个函数会触发
+};
+</script>
+<style  scoped>
+    .lp-toutiao-nav{
+        flex: 0 0 200px;
+        text-align: center;
+        margin-left: 100px;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+    h4{
+        font-size:20px;
+        color: #e36059;
+        font-weight: 200;
+        margin-bottom: 20px;
+    }
+    li {
+        font-size: 14px;
+        height: 30px;
+        line-height: 30px;
+        width: 100px;
+        border-radius: 6px;
+    }
+    .active{
+        color: #e36059;
+    }
+    li:hover{
+        background-color: #e36059;
+        color:white;
+        cursor: pointer;
+    }
+
+</style>
